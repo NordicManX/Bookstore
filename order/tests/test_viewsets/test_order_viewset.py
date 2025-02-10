f495065df32ad472b16394a72eb3711d31dd273e
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-02-10 14:06:43 -0300

    Adicionado o Token authentication e configurado o Django Rest Framework Token Authentication

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -25,10 +25,10 @@ class TestViewSet(APIClient):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         order_data = json.loads(response.content)[0]
-        self.assertEqual(order_data['product'][0]['title'], self.product.title)
-        self.assertEqual(order_data['product'][0]['price'], self.product.price)
-        self.assertEqual(order_data['product'][0]['active'], self.product.active)
-        self.assertEqual(order_data['product'][0]['category'][0]['title'], self.category.title)
+        self.assertEqual(order_data['result'][0]['product'][0]['title'], self.product.title)
+        self.assertEqual(order_data['result'][0]['product'][0]['price'], self.product.price)
+        self.assertEqual(order_data['result'][0]['product'][0]['active'], self.product.active)
+        self.assertEqual(order_data['result'][0]['product'][0]['category'][0]['title'], self.category.title)
         
         def test_create_order(self):
             user = UserFactory()
